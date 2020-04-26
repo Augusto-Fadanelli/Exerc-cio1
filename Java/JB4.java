6125e84114bf5d2f2be9279e5c663d753075fa2b
Augusto Fadanelli de Sousa <augustofadanelli.work@gmail.com>
2020-04-26 01:18:32 -0300

    Create JB4.java

---
+++ Java/JB4.java
@@ -0,0 +1,24 @@
+// 4 - Determine e exiba a média aritmética de um aluno a partir de suas 4 notas.
+// Calculo de média escolhido: (AP1 + AP2 + AP3 + AF)/4
+import java.util.Scanner;
+
+public class Questao4
+{
+    public static void main(String[] args)
+    {        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Insira a nota da AP1: ");
+        double AP1 = input.nextDouble();
+        System.out.print("Insira a nota da AP2: ");
+        double AP2 = input.nextDouble();
+        System.out.print("Insira a nota da AP3: ");
+        double AP3 = input.nextDouble();
+        System.out.print("Insira a nota da AF: ");
+        double AF = input.nextDouble();
+        
+        double media = (AP1 + AP2 + AP3 + AF)/4;
+        
+        System.out.println("A media aritmetica do aluno e: " + media);
+    }
+}
