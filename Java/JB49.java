5aef168ef8dec6e7651b048d0746f508f4382458
Augusto Fadanelli de Sousa <augustofadanelli.work@gmail.com>
2020-04-26 01:21:25 -0300

    Create JB49.java

---
+++ Java/JB49.java
@@ -0,0 +1,21 @@
+// 49 - Exiba os 50 primeiros números da seqüência de Fibonacci.
+
+public class Questao49
+{
+    public static void main(String[] args)
+    {        
+        int anterior = 1;
+        int atual = 1;
+        int aux;
+        
+        System.out.print(anterior + "; ");
+        
+        for(int i=0; i<50; i++){
+            System.out.print(atual + "; ");
+            
+            aux = atual;
+            atual += anterior;
+            anterior = aux;
+        }
+    }
+}
