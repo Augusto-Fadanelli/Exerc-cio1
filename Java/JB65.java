6a35279a587c83e2218958d876d09b84d44a6045
Augusto Fadanelli de Sousa <augustofadanelli.work@gmail.com>
2020-04-26 01:25:10 -0300

    Create JB65.java

---
+++ Java/JB65.java
@@ -0,0 +1,32 @@
+// 65 - Desenhe a seguinte pirâmide de asteriscos. O usuário determina a quantidade de linhas.
+import java.util.Scanner;
+
+public class Questao65
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Insira a quantidade de linhas: ");
+        int linhas = input.nextInt();
+        
+        for(int i=0, l=linhas; i<linhas; i++, l--){
+            //Linhas
+            for(int j=0; j<linhas; j++){
+                //colunas - Esquerda
+                if(j<l){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            for(int k=0; k<=i; k++){
+                //colunas - Direita
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+
+    }
+}
