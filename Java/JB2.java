7cc1d5da31a4b8f148e17da6f2fb97f13206f89e
Augusto Fadanelli de Sousa <augustofadanelli.work@gmail.com>
2020-04-26 01:16:42 -0300

    Create JB2.java

---
+++ Java/JB2.java
@@ -0,0 +1,19 @@
+// 2 - Calcule e exiba a quantidade de salários mínimos que um determinado funcionário ganha. Para isto, peça o valor do seu salário e o valor do salário mínimo atual.
+
+import java.util.Scanner;
+
+public class Questao2
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Insira o valor do salario mensal do funcionario em R$: ");
+        double salary = input.nextDouble();
+        System.out.print("Insira o valor do salario minimo em R$: ");
+        double minimum = input.nextDouble();
+        
+        double amount = salary/minimum;
+        System.out.println("O funcionario recebe " + amount + " salarios minimos!");
+    }
+}
