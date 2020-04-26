eeba1b703b75b48afb1e8af6bc7704cfd7196455
Augusto Fadanelli de Sousa <augustofadanelli.work@gmail.com>
2020-04-26 01:24:16 -0300

    Create JB64.java

---
+++ Java/JB64.java
@@ -0,0 +1,27 @@
+// 64 - Desenhe a seguinte seqüência de triângulos. O usuário determina a quantidade de triângulos.// 65 - Desenhe a seguinte pirâmide de asteriscos. O usuário determina a quantidade de linhas.
+import java.util.Scanner;
+
+public class Questao64
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Insira a quantidade de triangulos: ");
+        int triangulos = input.nextInt();
+        
+        for(int k=0; k<triangulos; k++){
+            //Triangulos
+            for(int i=0; i<=k; i++){
+                //Linhas
+                for(int j=0; j<=i; j++){
+                    //colunas
+                        System.out.print("*");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+    }
+}
