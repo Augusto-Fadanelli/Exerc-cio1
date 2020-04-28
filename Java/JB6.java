c7223b2a141450530e9aa17787282bb4df657f1d
Augusto Fadanelli de Sousa <augustofadanelli.work@gmail.com>
2020-04-27 21:51:25 -0300

    Update JB6.java

--- Java/JB6.java
+++ Java/JB6.java
@@ -1,14 +1,12 @@
 // 6 - Calcule e exiba área ocupada por um círculo cujo raio mede 5 metros. A área de um círculo é ¶ (PI) multiplicado pelo raio elevado ao quadrado. Em Java o valor de ¶ está disponível na constante Math.PI
 
 import java.util.Scanner;
-//import java.math.pi;
 
-public class Questao6
+public class JB6
 {
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
-        
+
         double r = 5; // Raio em metros
         double a = Math.PI*r*r; // Área
         
