970bd644c73b8bd3e72ad48dc1b443ddbf001cf0
Augusto Fadanelli de Sousa <augustofadanelli.work@gmail.com>
2021-04-26 22:57:22 -0300

    Create Ordenar.java

---
+++ Java/JB19/Ordenar.java
@@ -0,0 +1,15 @@
+public class Ordenar
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    float num1;
+    float num2;
+
+    void exibir(float n1, float n2)
+    {   
+        if(n1 >= n2){
+            System.out.printf("%f, %f\n", n1, n2);
+        }else{
+            System.out.printf("%f, %f\n", n2, n1);   
+        }
+    }
+}
